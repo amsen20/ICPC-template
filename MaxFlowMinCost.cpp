f92cd3b9c8bb260c21b45a3800e64b0822ab7f57
amsen20 <42866056+amsen20@users.noreply.github.com>
2021-11-12 19:23:05 +0330

    Fix max flow min cost.

--- MaxFlowMinCost.cpp
+++ MaxFlowMinCost.cpp
@@ -1,7 +1,7 @@
 // Running Complexity is about O(SPFA() * Max_flow) but better. Can be O(N^3M) using dijkstra.
-const int maxn=3000;
+const int maxn=4500;
 const int maxm=30*maxn;
-const int INF=1e5;
+const int INF=1e9;
 int head[maxn], to[maxm], prv[maxm], cap[maxm], cost[maxm], ecnt, source=maxn-2, sink=maxn-1;
 int q[maxm]; // Be cautious about this size of the SPFA queue.
 void init() {memset(head, -1, sizeof head); ecnt = 0;} // Call me.
@@ -11,7 +11,7 @@ void add_edge(int v, int u, int cst, int vu, int uv=0) {
 }
 int d[maxn], par[maxn];
 int spfa() {
-    memset(d, 31, sizeof d);
+    memset(d, 63, sizeof d);
     d[source] = 0;
     int h = 0, t = 0;
     q[t++] = source, par[source] = -1;
@@ -33,5 +33,5 @@ int mincost() {
     for (int tmp; (tmp=spfa()); flow+=tmp)
         for (int e = par[sink]; ~e; e = par[to[e ^ 1]])
             cap[e] -= tmp, cap[e ^ 1] += tmp, ans += cost[e] * tmp;
-    return flow*INF-ans;
+    return ans;
 }
